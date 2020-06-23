d9d1c585890a4c213f297d9720341b22188932e1
Junhao Cai <jhcai11.thu@gmail.com>
2020-06-23 01:13:39 +0100

    Added classes to hold constants

---
+++ SimMolFil/+simulation/Constants.m
@@ -0,0 +1,11 @@
+classdef Constants
+    %CONSTANTS Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties (Constant = true)
+        speed_of_light = 299792458;     	% m/s
+        c = 299792458;                      % m/s
+    end
+    
+end
+
